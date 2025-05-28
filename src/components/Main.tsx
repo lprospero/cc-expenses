26a685ac42066e0ec72434a39d9b174f3e02d30d
Lyle Prospero <lyle.prospero@gmail.com>
2025-05-28 20:47:28 +1000

    Removed side bar

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,9 @@
 import Header from '../components/Navigation/Header';
-import Sidebar from '../components/Navigation/Sidebar';
 import Dashboard from '../components/Navigation/Dashboard';
 
 const Main = () => {
   return (
     <div className="flex h-screen bg-gray-300">
-      <Sidebar />
       <div className="flex-1 flex flex-col rounded-lg m-2">
         <Header />
         <Dashboard />
--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -2,7 +2,8 @@ import AuthButtons from '../../components/AuthButtons';
 
 const Header = () => {
   return (
-    <header className="bg-blue-50 shadow p-4 flex items-center rounded-t-lg">
+    <header className="bg-blue-100 shadow p-4 flex items-center rounded-t-lg">
+      <h1 className="text-3xl font-bold text-gray-800">Expenses Dashboard</h1>
       <AuthButtons />
     </header>
   );
